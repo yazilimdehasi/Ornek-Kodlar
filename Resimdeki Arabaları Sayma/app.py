ae2076082f8ea809f723af7cd7ae2b1909a0a526
Yazılım Dehası <73071269+yazilimdehasi@users.noreply.github.com>
2021-05-01 17:34:09 +0300

    Add files via upload

---
+++ Resimdeki Arabaları Sayma/app.py
@@ -0,0 +1,11 @@
+import cv2
+import matplotlib.pyplot as plt
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+
+im = cv2.imread("arabalar.jpg")
+bbox, label, conf = cv.detect_common_objects(im)
+output_image = draw_bbox(im, bbox, label, conf)
+plt.imshow(output_image)
+plt.show()
+print("Resimde bulunan araba sayısı : " + str(label.count('car')))
